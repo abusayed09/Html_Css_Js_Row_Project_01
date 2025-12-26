ff0041b8179480d7d245281abe3675c1fa9058eb
abu sayed <mdabusayedbinamin@gmail.com>
2025-12-27 03:01:48 +0600

    update and Complete the Project

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+    document.addEventListener("DOMContentLoaded", function() {
+        const video = document.getElementById('bg-video');
+        const playBtn = document.getElementById('play-trigger');
+        const playIcon = document.getElementById('play-icon');
 
+        if (playBtn) {
+            playBtn.addEventListener('click', function() {
+                if (video.paused) {
+                    video.play();
+                    playIcon.className = 'fas fa-pause';
+                } else {
+                    video.pause();
+                    playIcon.className = 'fas fa-play';
+                }
+            });
+        }
+    });
